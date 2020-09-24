e2ab55815fa5bd71296051f31a87e75b771e30d0
Bhatt Akshita <Akshita.Bhatt@volvo.com>
2020-09-24 14:36:45 +0200

    corrected.
    Answer 233168

--- multiples_of_3_and_5.cpp
+++ multiples_of_3_and_5.cpp
@@ -7,7 +7,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.*/
   
 int multiples_of_3_and_5(){
 	int sum = 0;
-	for(int i = 1; i<1001; i++){
+	for(int i = 1; i<1000; i++){
 		if(i%3 == 0 or i%5 == 0){
 			sum = sum + i;
 		}
